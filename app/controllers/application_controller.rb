36a4e93f621a37e1ec012291b8769b2dfd44c856
Oyamada-2103 <ta9.syukatu@gmail.com>
2021-10-03 13:03:59 +0000

    devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  
+  # devise機能が使われる前にactionを実施する
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  
+  # before action
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  
 end
