b3dab59ae82d427c142cca3db0419b88ee889cd7
Oyamada-2103 <ta9.syukatu@gmail.com>
2021-10-05 13:13:49 +0000

    new/index

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,4 +3,6 @@ class PostImage < ApplicationRecord
   # 多：１
   belongs_to :user
   
+  # 画像アップ用メッソド
+attachment :image
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b360bf73345b12ec95fa7065b0ae2d02aef2c22743ea0eb85089809b6c802985833e562b6bc49da7c5fb87b17773be2857e0178c08ec4b2171b9dc2c150cfa74'
