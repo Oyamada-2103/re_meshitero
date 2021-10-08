41a90ee1e6a34847df4723cedfb3811456e27f7f
Oyamada-2103 <ta9.syukatu@gmail.com>
2021-10-08 12:42:18 +0000

    show/destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,17 @@
 class ApplicationController < ActionController::Base
-  
+
   # devise機能が使われる前にactionを実施する
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
+  def after_sign_in_path_for(resource)
+    post_images_path
+  end
+
   protected
-  
+
   # before action
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-  
+
 end
