848af229dd60f111214e748da063388e74a18544
Oyamada-2103 <ta9.syukatu@gmail.com>
2021-10-11 13:57:09 +0000

    favo model

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,10 +2,14 @@ class PostImage < ApplicationRecord
 
   # 多：１
   belongs_to :user
+  # 画像アップ用メッソド
+  attachment :image
 
   has_many :post_comments, dependent: :destroy
-
-
-  # 画像アップ用メッソド
-attachment :image
+  has_many :favorites, dependent: :destroy
+ 
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+ 
 end
